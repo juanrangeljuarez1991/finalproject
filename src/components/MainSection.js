3a94d5c305081adc71cdaa515a6b6449e24aff6a
juanrangeljuarez <juanrangel1@mail.weber.edu>
2024-12-26 13:35:42 -0700

    Working on grid

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,10 +1,21 @@
 import React from "react";
+import '../styles/menu.css'
 
 
 const MainSection = () => {
   return (
-
-          <p>Pete • © 2022</p>
+      <div className="mainMenu">
+          <a className="mainTittle"><b>This weeks specials</b></a>
+          <a><button type="button">Online Menu</button></a>
+          <br></br>
+          <div className="mainBody">
+            <div className="containerMenu">
+                  <div class="box A">Greek Salad</div>
+                  <div class="box B">Bruchets</div>
+                  <div class="box C">Lemon Dessert</div>
+            </div>
+          </div>
+      </div>
 
   );
 };
