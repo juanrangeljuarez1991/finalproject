3204b2d14c0fdac6d825630bb796c8b64a843b43
juanrangeljuarez <juanrangel1@mail.weber.edu>
2024-12-31 14:42:29 -0700

    Working on design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import Navbar from "./Nav";
+import '../styles/header.css'
 
 const Header = () => {
   
   return (
-
-  
     <>
-            
             <ul>
                  <head>
                      <title>Little Lemon</title>
@@ -17,12 +15,21 @@ const Header = () => {
                      <meta name="og:image" content="../images/logo.jpeg"/>
                  </head>
                  <a><Navbar /></a>
-  
+                 <div id="mainheader">
+                    <div id="textContainer">
+                            <h1 id="headerLittleLemon">Little Lemon</h1>
+                            <h2 id="subTitle">Chicago</h2>
+                            <p>We are a family owned Mediterranean restaurant, focused <br></br>
+                            on traditional recipes served with a modern twist</p>
+                            <a id="buttonreserve"><button type="button">Reserve a table</button></a>
+                    </div>
+                    <div id="imageContainers">
+                            <a><img src={require('../images/restauranfood.jpg')} alt="restaurant" width="150" height="250" /></a>
+                    </div>
+                 </div>
+
             </ul>
    </>
-
-    
-    
   );
 };
 export default Header;
--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -10,9 +10,33 @@ const MainSection = () => {
           <br></br>
           <div className="mainBody">
             <div className="containerMenu">
-                  <div class="box A">Greek Salad</div>
-                  <div class="box B">Bruchets</div>
-                  <div class="box C">Lemon Dessert</div>
+                  <div class="box A">
+                      <a><img src={require('../images/greek salad.jpg')} alt="restaurant" width="200" height="150" /></a>
+                      <div className="cardTitle">
+                        <h4>Greek salad</h4>
+                        <h4 id="cardTitlePrice">$12.99</h4>
+                      </div>
+                      <p className="itemDescription">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                      <h5>Order a delivery</h5>
+                  </div>
+                  <div class="box B">
+                        <a><img src={require('../images/bruchetta.jpeg')} alt="restaurant" width="200" height="150" /></a>
+                        <div className="cardTitle">
+                          <h4>Bruchetta</h4>
+                          <h4 id="cardTitlePrice">$5.99</h4>
+                        </div>
+                        <p className="itemDescription">Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
+                        <h5>Order a delivery</h5>
+                  </div>
+                  <div class="box C">
+                        <a><img src={require('../images/lemon dessert.jpg')} alt="restaurant" width="200" height="150" /></a>
+                        <div className="cardTitle">
+                          <h4>Lemon Dessert</h4>
+                          <h4 id="cardTitlePrice">$5.00</h4>
+                        </div>
+                        <p className="itemDescription">This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                        <h5>Order a delivery</h5>
+                  </div>
             </div>
           </div>
       </div>
